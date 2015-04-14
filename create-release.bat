07eee9ac41b7c0b2ab0432184408cac6e9382441
Brad Serbu <bradserbu@bradserbu.com>
2015-04-14 10:20:01 -0400

    Created release 2015-04-14.

--- create-release.bat
+++ create-release.bat
@@ -1,4 +1,4 @@
-@echo off
+echo off
 REM xcopy /F /D /S /Y bin\* releases\%current_date%\bin\
 
 REM -- Get the current date time in the format YYYY-MM-DD
@@ -6,15 +6,18 @@ set current_date=%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%
 set release_dir=releases\%current_date%\
 
 REM -- Update with contents from bin folder and copy to release directory
-lib\utils\echoc 2 Creating release binaries... %release_dir%
+lib\utils\echoc 2 Creating release binaries...
 mkdir %release_dir%bin\
 lib\utils\echoc 2 Copying \bin...
-robocopy bin\ %release_dir%bin\ /MIR /XD bin\data
+robocopy bin %release_dir%bin /MIR /XD data logs
 
 REM -- Create lib
 lib\utils\echoc 2 Creating release lib...
-mkdir releases\%current_date%\lib\
-lib\ILMerge\ILMerge.exe /v4 /out:releases\%current_date%\lib\solumlib.dll /wildcards bin\solum.*.dll bin\solum.exe bin\RaptorDB*.dll bin\Handlebars.dll bin\Newtonsoft.Json.dll bin\NLog.dll bin\System.Threading.Tasks.Dataflow.dll
+mkdir %release_dir%lib\
+lib\ILMerge\ILMerge.exe /v4 /out:%release_dir%lib\solumlib.dll /wildcards bin\solum.*.dll bin\solum.exe bin\RaptorDB*.dll bin\Handlebars.dll bin\Newtonsoft.Json.dll bin\NLog.dll bin\System.Threading.Tasks.Dataflow.dll
+
+REM -- Display git status
+git status
 
 REM -- Git Add
 echo.
@@ -23,7 +26,7 @@ set /p git_add="Git add release? [y]/n "
 
 if "%git_add:~0,1%" equ "y" (
 	lib\utils\echoc 2 Adding...
-	git add -Af %release_dir%	
+	git add -Af %release_dir%
 ) else (
 	goto COMPLETED
 )
@@ -35,7 +38,7 @@ set /p git_commit="Git commit release? [y]/n "
 
 if "%git_commit:~0,1%" equ "y" (
 	lib\utils\echoc 2 Commiting...
-	git commit -am "Creating release %current_date%"
+	git commit -am "Created release %current_date%."
 ) else (
 	goto COMPLETED
 )
@@ -50,8 +53,8 @@ if "%git_commit:~0,1%" equ "y" (
 	git push
 ) else (
 	goto COMPLETED
-)	
+)
 	
 :COMPLETED
 echo.
-lib\utils\echoc 2 Sucessfully created release %current_date%!
+lib\utils\echoc 2 Sucessfully created release %current_date%.
