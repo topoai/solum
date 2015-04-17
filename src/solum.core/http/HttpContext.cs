a3e69f38a2b9d8b1082f30fc267930548d2bf0b4
Brad Serbu <bradserbu@bradserbu.com>
2015-04-17 17:45:04 -0400

    Updates after working through the kinks with the YikYak collector

---
+++ src/solum.core/http/HttpContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solum.core.http
+{
+    public class HttpContext
+    {
+        public HttpContext(long requestNum, HttpListenerContext context)
+        {
+            this.RequestNum = requestNum;
+            this.Context = context;
+            this.m_timer = Stopwatch.StartNew();
+            this.IsCompleted = false;
+        }
+
+        Stopwatch m_timer;
+
+        bool IsCompleted { get; private set; }
+        public long RequestNum { get; private set; }        
+        public HttpListenerContext Context { get; private set; }
+        public TimeSpan Elapsed { get { return m_timer.Elapsed; } }
+        public void Complete()
+        {
+            m_timer.Stop();
+            IsCompleted = true;
+        }
+    }
+}
